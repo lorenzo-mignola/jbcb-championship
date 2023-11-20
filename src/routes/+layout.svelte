3dd8a4e57935dbbd34ac7ee819f29bde5d1195eb
lorenzo-mignola <lo.mignola@gmail.com>
2023-11-20 18:49:27 +0100

    chore: setup pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+  import { pwaInfo } from 'virtual:pwa-info';
   import '../app.postcss';
+
+  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 
   const title = '🥋 JBCB Championship';
 </script>
+
+<svelte:head>
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  {@html webManifestLink}
+</svelte:head>
 
 <AppBar slotTrail="place-content-end" background="bg-surface-100-800-token" class="sticky top-0">
   <a href="/"><h1 class="text-3xl font-bold">{title}</h1></a>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,41 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss()],
+  plugins: [
+    sveltekit(),
+    purgeCss(),
+    SvelteKitPWA({
+      scope: '/',
+      base: '/',
+      manifest: {
+        short_name: 'JBCB Championship',
+        name: 'JBCB Championship',
+        theme_color: '#7f2f3b',
+        background_color: '#7f2f3b',
+        icons: [
+          {
+            src: '/192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   test: {
     include: ['**/*.test.ts']
   }
