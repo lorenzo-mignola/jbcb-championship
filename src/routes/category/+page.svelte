3339cba1badea01f5e18b00401b52139ed615835
lorenzo-mignola <lo.mignola@gmail.com>
2023-11-24 18:31:50 +0100

    feat: add match page

--- src/routes/category/+page.svelte
+++ src/routes/category/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import AthleteList from '$lib/components/AthleteList.svelte';
+  import Next from '../../icons/next.svelte';
   import { getCategory } from '../../lib/db/methods';
   import Loading from './loading.svelte';
   import Stats from './stats.svelte';
@@ -12,7 +13,16 @@
   {#if !category}
     <Loading />
   {:else}
-    <h2 class="h2 font-bold mb-10">{category.name}</h2>
+    <div class="flex justify-between">
+      <h2 class="h2 font-bold mb-10">{category.name}</h2>
+      <div>
+        <a href={`/category/match?id=${category.id}`}>
+          <button class="btn btn-lg variant-filled-primary"
+            >Inizia <span class="ml-2"><Next /></span></button
+          >
+        </a>
+      </div>
+    </div>
     <AthleteList athletes={category.athletes}></AthleteList>
     <Stats {category} />
   {/if}
---
+++ src/routes/category/match/+page.svelte
@@ -0,0 +1,10 @@
+<div>Nome categoria</div>
+<div class="judoka-white p-4 flex justify-between text-3xl font-bold">
+  <div>Nome</div>
+  <div>0</div>
+</div>
+<div class="judoka-blue p-4 flex justify-between text-3xl font-bold">
+  <div>Nome</div>
+  <div>0</div>
+</div>
+<div class="w-full text-center p-4 text-5xl m-2">04:00</div>
