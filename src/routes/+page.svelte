0b52484b81bd1c10c17b780e733a1489597e9169
lorenzo-mignola <lo.mignola@gmail.com>
2024-01-05 15:26:26 +0100

    fix: remove page error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,9 @@
 <script>
   import Settings from '$lib/icons/settings.svelte';
   import Card from './card.svelte';
-
-  export let data;
 </script>
 
 <div class="flex flex-col gap-3">
-  {JSON.stringify(data.c)}
   <Card link="/new?category-name=" text="Crea categoria" />
   <Card link="/categories" text="Visualizza tutte le categorie" />
   <a class="btn variant-ringed-primary" href="/settings">
--- src/routes/+page.ts
+++
@@ -1,9 +0,0 @@
-import { getAllCategories } from '../lib/db/methods';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async () => {
-  const c = await getAllCategories();
-  return {
-    c
-  };
-};
