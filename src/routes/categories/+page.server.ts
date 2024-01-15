196cd840359e79985d61b8227a5be153e87d2a61
lorenzo-mignola <lo.mignola@gmail.com>
2024-01-15 21:28:13 +0100

    chore: optimize data load

--- src/routes/categories/+page.server.ts
+++ src/routes/categories/+page.server.ts
@@ -5,6 +5,10 @@ export const load: PageServerLoad = async ({ url }) => {
   const tournament = url.searchParams.get('tournament');
   const categories = await getAllCategories(tournament || '');
   return {
-    categories
+    categories: categories.map((category) => ({
+      id: category.id,
+      name: category.name,
+      currentMatch: category.currentMatch
+    }))
   };
 };
