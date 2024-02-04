dbc839a576777ff5b731af3f8360d7c0fde80792
lorenzo-mignola <lo.mignola@gmail.com>
2024-02-04 16:06:38 +0100

    feat: add wake lock on timer

--- src/lib/components/match/timer.svelte
+++ src/lib/components/match/timer.svelte
@@ -2,11 +2,13 @@
   import TimerView from '$lib/components/match/timer-view.svelte';
   import { oseakomiType, timerOsaekomi } from '$lib/components/osaekomi/$osaekomi-timer';
   import { getMin, getSec, isGoldenScore, timer } from '$lib/store/$timer';
+  import WakeLock from '$lib/utils/wake-lock.svelte';
 
   $: min = getMin($timer);
   $: sec = getSec($timer);
 </script>
 
+<WakeLock />
 <TimerView
   isGoldenScore={$isGoldenScore}
   {min}
