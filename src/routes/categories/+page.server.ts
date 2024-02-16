b776ca51c20b14a3ca2a59080818856a0e0d6438
lorenzo-mignola <lo.mignola@gmail.com>
2024-02-16 18:03:19 +0100

    feat: sort category name

--- src/routes/categories/+page.server.ts
+++ src/routes/categories/+page.server.ts
@@ -1,14 +1,16 @@
+import { prop, sortBy } from 'ramda';
 import { getAllCategories } from '../../lib/server/methods';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
   const tournament = url.searchParams.get('tournament');
   const categories = await getAllCategories(tournament || '');
+  const categoriesMapped = categories.map((category) => ({
+    id: category.id,
+    name: category.name,
+    currentMatch: category.currentMatch
+  }));
   return {
-    categories: categories.map((category) => ({
-      id: category.id,
-      name: category.name,
-      currentMatch: category.currentMatch
-    }))
+    categories: sortBy<(typeof categoriesMapped)[number]>(prop('name'))(categoriesMapped)
   };
 };
