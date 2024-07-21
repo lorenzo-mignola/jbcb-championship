b30d52216715b3ca4f3a114bf8149024a6b42b96
lorenzo-mignola <lo.mignola@gmail.com>
2024-07-21 14:41:50 +0200

    refactor: use map for keys

--- src/lib/shortcuts/get-key.ts
+++ src/lib/shortcuts/get-key.ts
@@ -1,84 +1,35 @@
 import type { JudokaType } from '../types/match.type';
 
-type ShortcutType = 'play' | 'ippon' | 'wazari' | 'shido' | 'osaekomi' | '';
+type ShortcutJudokaType = 'ippon' | 'wazari' | 'shido' | 'osaekomi';
+type ShortcutType = 'play' | ShortcutJudokaType;
 
-const isWhite = (judokaType: JudokaType) => judokaType === 'white';
-const isBlue = (judokaType: JudokaType) => judokaType === 'blue';
-
-const playKey = () => ' ';
-
-const ipponKey = (judokaType: JudokaType) => {
-  if (isWhite(judokaType)) {
-    return 'A';
+const keys: Record<ShortcutJudokaType, Record<JudokaType, string>> = {
+  ippon: {
+    white: 'A',
+    blue: 'H'
+  },
+  wazari: {
+    white: 'S',
+    blue: 'J'
+  },
+  shido: {
+    white: 'D',
+    blue: 'K'
+  },
+  osaekomi: {
+    white: 'F',
+    blue: 'L'
   }
-
-  if (isBlue(judokaType)) {
-    return 'H';
-  }
-
-  return '';
-};
-
-const wazariKey = (judokaType: JudokaType) => {
-  if (isWhite(judokaType)) {
-    return 'S';
-  }
-
-  if (isBlue(judokaType)) {
-    return 'J';
-  }
-
-  return '';
-};
-
-const shidoKey = (judokaType: JudokaType) => {
-  if (isWhite(judokaType)) {
-    return 'D';
-  }
-
-  if (isBlue(judokaType)) {
-    return 'K';
-  }
-
-  return '';
-};
-
-const osaekomiKey = (judokaType: JudokaType) => {
-  if (isWhite(judokaType)) {
-    return 'F';
-  }
-
-  if (isBlue(judokaType)) {
-    return 'L';
-  }
-
-  return '';
 };
 
 export const getKey = (shortcutType: ShortcutType) => (judokaType?: JudokaType) => {
   if (shortcutType === 'play') {
-    return playKey();
+    return ' ';
   }
 
   if (!judokaType) {
     throw new Error(`Missing judoka type for shortcut ${shortcutType}`);
   }
 
-  if (shortcutType === 'ippon') {
-    return ipponKey(judokaType);
-  }
-
-  if (shortcutType === 'wazari') {
-    return wazariKey(judokaType);
-  }
-
-  if (shortcutType === 'shido') {
-    return shidoKey(judokaType);
-  }
-
-  if (shortcutType === 'osaekomi') {
-    return osaekomiKey(judokaType);
-  }
-
-  return '';
+  return keys[shortcutType][judokaType];
 };
